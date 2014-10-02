bfe876336203e466080ed1a12f35c0268747a76d
HGtz2222 <727706910@qq.com>
2014-10-02 22:04:36 +0800

    创建项目, 创建主界面

---
+++ WhoIsUndercover/src/com/tz/whoisundercover/MainActivity.java
@@ -0,0 +1,58 @@
+package com.tz.whoisundercover;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private Button btnStartGame;
+	private Button btnSeeRules;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        initUI();
+    }
+
+
+    private void initUI() {
+    	btnStartGame = (Button)findViewById(R.id.btn_start_game);
+    	btnStartGame.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				Log.e("tz", "开始游戏!");
+				Intent intent = new Intent(MainActivity.this, ConfigGame.class);
+				startActivity(intent);
+			}
+    		
+    	});
+    	
+    	btnSeeRules = (Button)findViewById(R.id.btn_rules);
+    	btnSeeRules.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				Log.e("tz", "查看规则");
+				Intent intent = new Intent(MainActivity.this, RulesGame.class);
+				startActivity(intent);
+			}
+    		
+    	});
+	}
+
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
---
+++ WhoIsUndercover/src/com/tz/whoisundercover/Ring.java
@@ -0,0 +1,12 @@
+package com.tz.whoisundercover;
+
+import android.app.Activity;
+import android.app.Service;
+import android.os.Vibrator;
+
+public class Ring {
+	public static void ring(final Activity activity){
+		Vibrator vib = (Vibrator)activity.getSystemService(Service.VIBRATOR_SERVICE);
+		vib.vibrate(100);
+	}
+}
